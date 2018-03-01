e5758b34ab99634fdd0fda977360147de64fe406
Cloud Raxak <raxak@cloudraxak.com>
2018-03-01 19:18:55 +0530

    static

--- remmon/settings.py
+++ remmon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kcgi6nnzrr*z4_%08558lxci)+8ew7*qn(kcafk@xml$j&jp4v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
