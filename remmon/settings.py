e71ddc28e35035d1da79c9abeeeeadc68ed6fb96
Cloud Raxak <raxak@cloudraxak.com>
2018-03-01 18:51:09 +0530

    static settings

--- remmon/settings.py
+++ remmon/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/tmp/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Add static folder to STATIC_DIRS
 STATICFILES_DIRS = [
-    'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
