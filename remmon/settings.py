1209e58bffa9147463f017bf3d7f6677854ac99d
Cloud Raxak <raxak@cloudraxak.com>
2018-03-01 19:13:54 +0530

    static

--- remmon/settings.py
+++ remmon/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Add static folder to STATIC_DIRS
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
